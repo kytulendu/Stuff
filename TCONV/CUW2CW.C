8eba35a0b87f70c6dabc9c3f5f00c60f2447d16e
Khralkatorrix <garuda2550@gmail.com>
2025-05-17 15:52:58 +0700

    Add Computer Union Word to CU-Writer converter.
    Signed-off-by: Khralkatorrix <garuda2550@gmail.com>

---
+++ TCONV/CUW2CW.C
@@ -0,0 +1,82 @@
+/*
+* ============================================================================
+* Convert Computer Union Word to CU-Writer, TIS-620 only.
+* By Khralkatorrix <https://github.com/kytulendu>.
+*
+* This is free and unencumbered software released into the public domain.
+*
+* Anyone is free to copy, modify, publish, use, compile, sell, or
+* distribute this software, either in source code form or as a compiled
+* binary, for any purpose, commercial or non-commercial, and by any
+* means.
+
+* In jurisdictions that recognize copyright laws, the author or authors
+* of this software dedicate any and all copyright interest in the
+* software to the public domain. We make this dedication for the benefit
+* of the public at large and to the detriment of our heirs and
+* successors. We intend this dedication to be an overt act of
+* relinquishment in perpetuity of all present and future rights to this
+* software under copyright law.
+
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*
+* For more information, please refer to <http://unlicense.org/>
+*
+* ============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cuw2cw.h"
+
+void usage(void)
+{
+    puts("Convert Computer Union Word to CU-Writer, TIS-620 only.");
+    puts("By Khralkatorrix.\n\n");
+    puts("Usage: CUW2CW [input file] [output file]");
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *inFile, *outFile;
+    unsigned char character;
+
+    if (argc != 3)
+    {
+        usage();
+        exit(0);
+    }
+
+    if ((inFile = fopen(argv[1], "r")) == NULL)
+    {
+        puts("Can't open input file.");
+        exit(0);
+    }
+    if ((outFile = fopen(argv[2], "w")) == NULL)
+    {
+        puts("Can't open output file.");
+        exit(0);
+    }
+
+    do {
+        character = fgetc(inFile);
+        if (feof(inFile))
+        {
+            break;
+        }
+        fprintf(outFile, "%c", cuw2std(character));
+    } while (1);
+
+    puts("Finished!");
+
+    fclose(inFile);
+    fclose(outFile);
+    return 0;
+}
